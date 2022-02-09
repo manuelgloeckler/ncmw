bbb9ac8b38462186fdad411f2c226309a358cea8
manuelgloeckler <38903899+manuelgloeckler@users.noreply.github.com>
2022-02-09 11:51:39 +0100

    ff

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -17,7 +17,7 @@ set -x
 ###################
  
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-nbsphinx python3-m2r2
+apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-nbsphinx m2r
 
  
 #####################
